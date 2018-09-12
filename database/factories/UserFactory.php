627d4a8eeafb9d7b42c9db210f852398bb6ca67d
vagrant <vagrant@homestead>
2018-09-12 13:39:42 +0000

    完善假数据填充

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -17,7 +17,8 @@ $factory->define(App\Models\User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
-        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+        'password' => '$2y$10$52xlGAZTrd7LivF7nyBvIeHulFfuzrkkSUcK5tA/lo2aO6AmcCPfW', // 123456
         'remember_token' => str_random(10),
+        'email_verified' => true,
     ];
 });
